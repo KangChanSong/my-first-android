484f322012c53a7d87481ca54e4f734aec72747b
KangChanSong <thdrkdcks@naver.com>
2024-07-13 19:28:55 +0900

    [ADD] Column , Row 컴포저블 사용 & 버튼 추가 (material3)

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfirstandroidapp.ui.theme.MyFirstAndroidAppTheme
@@ -30,26 +31,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting(name = "Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("KangChan", "GangGeon")
+) {
+    Column(modifier = modifier.padding()) {
+        names.forEach { Greeting(name = it) }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello my name is $name!",
-            modifier = modifier
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 8.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = modifier.weight(1f)
+            ) {
+                Text(text = "Hello", modifier)
+                Text(text = "$name!")
+            }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
+
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     MyFirstAndroidAppTheme {
